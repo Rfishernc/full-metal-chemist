6482b1bc4d88285f021a45e238d1b7f812dd1621
RichFisher <rich.fishernc@gmail.com>
2018-10-06 10:45:20 -0500

    property setter method created and added to existing methods

--- main.js
+++ main.js
@@ -8,6 +8,80 @@ class molecule {
         this.atoms = '';
     }
 
+    propertySetter() {
+        function weightSummation() {
+            let molecularWeight = 0;
+            for(let i = 0; i < this.branch.length; i++) {
+                for(let j = 0; j < this.branch[i].atomicStructure.length; j++) {
+                    molecularWeight += this.branch[i].atomicStructure[j].weight;
+                }
+            }
+            return molecularWeight;
+        }
+        let boundWeight = weightSummation.bind(this);
+        this.molecularWeight = boundWeight();
+    
+        function formulaSummation() {
+            let formula = 0;
+            for(let i = 0; i < this.branch.length; i++) {
+                for(let j = 0; j < this.branch[i].atomicStructure.length; j++) {
+                    formula += this.branch[i].atomicStructure[j].element;
+                }
+            }
+            return formula;
+        }
+        let boundFormula = formulaSummation.bind(this);
+        this.formula = boundFormula();
+   
+        let Ccount = (this.formula.match(/C/g) || []).length; 
+        let Hcount = (this.formula.match(/H/g) || []).length;
+        let Ocount = (this.formula.match(/O/g) || []).length;
+        let Bcount = (this.formula.match(/B/g) || []).length;
+        let Brcount = (this.formula.match(/Br/g) || []).length;
+        let Clcount = (this.formula.match(/Cl/g) || []).length;
+        let Fcount = (this.formula.match(/F/g) || []).length;
+        let Mgcount = (this.formula.match(/Mg/g) || []).length;
+        let Ncount = (this.formula.match(/N/g) || []).length;
+        let Pcount = (this.formula.match(/P/g) || []).length;
+        let Scount = (this.formula.match(/S/g) || []).length;
+        Bcount = Bcount - Brcount;
+        
+        let Cformula = 'C' + Ccount;
+        let Hformula = 'H' + Hcount;
+        let Oformula = 'O' + Ocount;
+        let Bformula = 'B' + Bcount;
+        let Brformula = 'Br' + Brcount; 
+        let Clformula = 'Cl' + Clcount;
+        let Fformula = 'F' + Fcount;
+        let Mgformula = 'Mg' + Mgcount;
+        let Nformula = 'N' + Ncount;
+        let Pformula = 'P' + Pcount;
+        let Sformula = 'S' + Scount;
+
+        let formulaArray = [Cformula, Hformula, Oformula, Bformula, Brformula, Clformula, Fformula, Mgformula, Nformula, Pformula, Sformula];
+        let countArray = [Ccount, Hcount, Ocount, Bcount, Brcount, Clcount, Fcount, Mgcount, Ncount, Pcount, Scount];
+        let formula = '';
+
+        for(let i = 0; i < formulaArray.length; i++) {
+            if(countArray[i] > 0) {
+                formula += formulaArray[i];
+            }
+        }
+        this.formula = formula;
+    
+        function valenceSummation() {
+            let availableValence = 0;
+            for(let i = 0; i < this.branch.length; i++) {
+                for(let j = 0; j < this.branch[i].atomicStructure.length; j++) {
+                    availableValence += this.branch[i].atomicStructure[j].valence;
+                }
+            }
+            return availableValence;
+        }
+        let boundValence = valenceSummation.bind(this);
+        this.availableValence = boundValence();
+    }
+
     brancher() {
         for(let i = 0; i < arguments.length; i++) {
             let carbonQty = arguments[i];
@@ -20,6 +94,7 @@ class molecule {
                 this.counter++;
             }
         }
+        this.propertySetter();
     }
 
     bounder() {
@@ -28,6 +103,7 @@ class molecule {
             this.branch[bonder[1]-1].atomicStructure[bonder[0]-1].carbonBonds = (('Branch'+bonder[3]) + ' ' + ('C' + bonder[2]));
             this.branch[bonder[3]-1].atomicStructure[bonder[2]-1].carbonBonds = (('Branch'+bonder[1]) + ' ' + ('C' + bonder[0]));
         }
+        this.propertySetter();
     }    
 
     mutate() {
@@ -37,6 +113,7 @@ class molecule {
             window[mutator[2]+mutatorId] = new atom(mutator[2], mutatorId);
             this.branch[mutator[1]-1].atomicStructure.splice(mutator[0], 1,  window[mutator[2]+mutatorId]); 
         }
+        this.propertySetter();
     }
     
     add() {
@@ -47,6 +124,7 @@ class molecule {
             this.branch[adder[1]-1].atomicStructure.splice(adder[0],0, window[adder[2]+counter]);
             this.counter++;
         }
+        this.propertySetter();
     }
 
     add_chaining() {
@@ -57,22 +135,13 @@ class molecule {
             this.branch[arguments[1]-1].atomicStructure.splice((arguments[0]+i-2),0, window[chainer+counter]);
             this.counter++;
         }
+        this.propertySetter();
     }
 
     
 
     closer() {
-        function valenceSummation() {
-            let availableValence = 0;
-            for(let i = 0; i < this.branch.length; i++) {
-                for(let j = 0; j < this.branch[i].atomicStructure.length; j++) {
-                    availableValence += this.branch[i].atomicStructure[j].valence;
-                }
-            }
-            return availableValence;
-        }
-        let boundValence = valenceSummation.bind(this);
-        this.availableValence = boundValence();
+        this.propertySetter();
     }
 
     unlock() {
@@ -115,3 +184,29 @@ m.add_chaining(2,2,'H','S','F');
 console.log(m.branch);
 m.closer();
 console.log(m);
+
+// propertySetter() {
+//     function weightSummation() {
+//     let molecularWeight = 0;
+//     for(let i = 0; i < this.branch.length; i++) {
+//         for(let j = 0; j < this.branch[i].atomicStructure.length; j++) {
+//             molecularWeight += this.branch[i].atomicStructure[j].weight;
+//         }
+//     }
+//     return molecularWeight;
+// }
+// let boundWeight = weightSummation.bind(this);
+//         this.molecularWeight = boundWeight();
+
+// function formulaSummation() {
+//     let formula = 0;
+//     for(let i = 0; i < this.branch.length; i++) {
+//         for(let j = 0; j < this.branch[i].atomicStructure.length; j++) {
+//             formula += this.branch[i].atomicStructure[j].element;
+//         }
+//     }
+//     return formula;
+// }
+// let boundFormula = formulaSummation.bind(this);
+//         this.formula = boundFormula();
+// }
